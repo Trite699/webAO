eb4dbe55afef3ac9c918d014e36d17b954bcd93a
Phoenix Trite <161424109+Trite699@users.noreply.github.com>
2026-08-14 21:27:40 +0530

    Update global.d.ts

--- webAO/global.d.ts
+++ webAO/global.d.ts
@@ -6,13 +6,16 @@ declare global {
     banPlayer: (id: number) => void;
     changeBlipVolume: () => void;
     changeMusicVolume: (volume?: number) => void;
+    deleteLocalCharacterUI: (name: string) => Promise<void>;
     getIndexFromSelect: (select_box: string, value: string) => number;
+    importLocalCharacterZip: () => Promise<void>;
     kickPlayer: (id: number) => void;
     onReplayGo: (_event: Event) => void;
     opusCheck: (channel: HTMLAudioElement) => OnErrorEventHandlerNonNull;
     pickEmotion: (emo: number) => void;
     pickEvidence: (evidence: number) => void;
     reloadTheme: () => void;
+    renderLocalCharacterList: () => void;
     resizeChatbox: () => void;
     setChatbox: (setstyle: string) => void;
     showname_click: (_event: Event | null) => void;
@@ -20,5 +23,6 @@ declare global {
     updateActionCommands: (side: string) => void;
     updateBackgroundPreview: () => void;
     updateTypingIndicator: () => void;
+    useLocalCharacter: (name: string) => void;
   }
 }
